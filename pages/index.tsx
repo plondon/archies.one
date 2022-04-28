d82789166e9c17a6a0b7c9b6dc39b713f0c9103c
plondon <philip.n.london@gmail.com>
2022-04-27 23:29:15 -0400

    Remove powered by vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,19 +40,6 @@ const Home: NextPage = () => {
           <Image src={Clouds} layout='fill' objectFit="contain" />
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };

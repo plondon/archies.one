02224869cf1812b92dd04940978e6c1c5533df66
plondon <philip.n.london@gmail.com>
2022-09-11 13:28:54 -0400

    Change learn page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,23 +1,28 @@
-import Image from 'next/image'
-import React from 'react'
-import UnderConstruction from '../../public/danger-hard-hat.png'
+import Image from "next/image";
+import React from "react";
 
 const Learn: React.FC<Props> = () => {
   return (
-    <div className='text-center text-2xl pt-6 max-w-xl m-auto'>
-        <p>
-            Archie&#8217;s website should updated every 1 or 2 weeks with new things. For example photos.
-        </p>
-        <p className='pt-2'>Archie&#8217;s website is currently under construction. It will be updated when it&#8217;s finished.</p>
-        <div className='mt-6 relative h-48 w-72 m-auto'>
-            <Image src={UnderConstruction} layout='fill' objectFit='contain' />
-        </div>
+    <div className="text-center text-2xl pt-6 max-w-xl m-auto">
+      <p>This website is run by Archie Greene.</p>
+      <p>Hi my name is Archie and I am 7 years old.</p>
+      <p>
+        Archie&#8217;s website should updated every 1 or 2 weeks with new
+        things. For example photos.
+      </p>
+      <div className="mt-8">
+        <Image
+          alt="archie"
+          src={"/items/archie-first-day.png"}
+          layout="fixed"
+          width={300}
+          height={321}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-type Props = {
-  
-}
+type Props = {};
 
-export default Learn
+export default Learn;

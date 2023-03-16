f8f163487d4dfe813362c91916d0a39373443bd7
plondon <philip.n.london@gmail.com>
2023-03-16 19:23:15 -0400

    Add more words

--- pages/learn-french/index.tsx
+++ pages/learn-french/index.tsx
@@ -29,6 +29,84 @@ const LearnFrench: React.FC<Props> = () => {
             <li>monkey=singe pronounced: se-nj</li>
             <li>lion=lion pronounced: lee-on</li>
             <li>zebra=zèbre pronounced: zeb-r</li>
+            <li>giraffe=girafe pronounced: jee-raf</li>
+            <li>snake=serpent pronounced: ser-pahn</li>
+            <li>crocodile=crocodile pronounced: kroh-koh-dil</li>
+            <li>alligator=alligator pronounced: al-ee-ga-tor</li>
+            <li>hippopotamus=hippopotame pronounced: hip-oh-poh-tahm</li>
+            <li>rhinoceros=rhinocéros pronounced: rhee-no-seh-ros</li>
+            <li>ant=fourmi pronounced: foor-mee</li>
+            <li>bee=abeille pronounced: ab-ay</li>
+            <li>spider=araignée pronounced: ar-ah-nee</li>
+            <li>fly=mouche pronounced: moo-sh</li>
+            <li>mosquito=moustique pronounced: moo-stick</li>
+            <li>ladybug=coccinelle pronounced: koh-sin-ell</li>
+            <li>beetle=scarabée pronounced: skah-ra-bay</li>
+            <li>grasshopper=sauterelle pronounced: saw-tay-rell</li>
+            <li>ant=escargot pronounced: es-car-go</li>
+
+
+          </ul>
+          <ul className="mt-4">
+            <li>today&apos;s subject is food 3/16/23</li>
+            <li>sushi-sushi-sushi</li>
+            <li>rice=riz pronounced: reez</li>
+            <li>fish=poisson pronounced: pwah-sawn</li>
+            <li>salmon=saumon pronounced: saw-mawn</li>
+            <li>shrimp=crevette pronounced: krev-et</li>
+            <li>lobster=homard pronounced: oh-mar</li>
+            <li>crab=crabe pronounced: krah-b</li>
+            <li>clam=huître pronounced: oo-tre</li>
+            <li>oyster=huitre pronounced: oo-tre</li>
+            <li>octopus=poisson-pierre pronounced: pwah-sawn-pea</li>
+            <li>egg=oeuf pronounced: oh-ef</li>
+            <li>bread=pain pronounced: pahn</li>
+            <li>cheese=fromage pronounced: froh-mahzh</li>
+            <li>meat=viande pronounced: vee-ahn</li>
+            <li>beef=bœuf pronounced: boof</li>
+            <li>pork=porc pronounced: por</li>
+            <li>chicken=poulet pronounced: poul-ay</li>
+            <li>lamb=mouton pronounced: moo-tun</li>
+            <li>ham=jambon pronounced: zhahm-bon</li>
+            <li>sausage=saucisse pronounced: saw-sees</li>
+            <li>bacon=bacon pronounced: bah-con</li>
+            <li>steak=bœuf à point pronounced: boof ah pwa</li>
+            <li>vegetable=légume pronounced: leh-goom</li>
+            <li>potato=patate pronounced: pah-tah-t</li>
+            <li>carrot=carotte pronounced: kah-roht</li>
+            <li>onion=oignon pronounced: wa-nahn</li>
+            <li>garlic=ail pronounced: ah</li>
+            <li>tomato=tomate pronounced: toh-mah-t</li>
+            <li>lettuce=laitue pronounced: lye-t</li>
+            <li>broccoli=brocoli pronounced: broh-koh-lee</li>
+            <li>asparagus=asperge pronounced: as-perzh</li>
+            <li>pepper=poivre pronounced: pwah-vr</li>
+            <li>peanut=arachide pronounced: ah-rah-chee-day</li>
+            <li>chocolate=chocolat pronounced: shoh-koh-lah</li>
+            <li>coffee=café pronounced: kah-fay</li>
+            <li>tea=thé pronounced: teh</li>
+            <li>water=eau pronounced: oh</li>
+            <li>juice=jus pronounced: zhoo</li>
+            <li>milk=lait pronounced: lye</li>
+            <li>beer=bière pronounced: bee-air</li>
+            <li>wine=vin pronounced: vahn</li>
+            <li>champagne=champagne pronounced: shahm-pahn</li>
+            <li>whiskey=whisky pronounced: whiss-kee</li>
+            <li>rum=rhum pronounced: rahm</li>
+            <li>gin=gin pronounced: jin</li> 
+            <li>vodka=vodka pronounced: vod-ka</li>
+            <li>liqueur=liqueur pronounced: lee-kur</li>
+            <li>lemon=citron pronounced: see-trohn</li>
+            <li>orange=orange pronounced: oh-ran-zh</li>
+
+
+
+
+
+
+
+
+              
           </ul>
         </div>
         <div className="flex justify-center w-full items-center">
